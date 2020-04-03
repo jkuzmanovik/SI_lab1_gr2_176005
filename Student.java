cff5d3edf2a553ac7f300d8ad0b3e1133b04b2b3
jkuzmanovik <jordan.kuzmanovik@gmail.com>
2020-04-03 19:49:43 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,55 @@
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
 
-	//TODO constructor
+	public Student(String i,String first,String last){
+		index = i;
+		firstName = first;
+		lastName = last; 
+	}
+	String getIndex(){
+		return this.index;
+	}
+	String getFirstName(){
+		return this.firstName;
+	}
+	
+	String getLastName(){
+		return this.lastName;
+	}
 
-	//TODO seters & getters
+	void setFirstName(String name){
+		firstName = name;
+	}
+	void setLastName(String last){
+		lastName = last;
+	}
+	void setIndex(String i){
+		index = i;
+	}
+
+
 
 	public double getAverage() {
-		//TODO
+		double sum = 0;
+		for (int i=0;i<grades.length;i++){
+			sum+=grades[i];
+		}
+		return (sum/grades.length);
+
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		int krediti = 0;
+		for (int i=0;i<grades.length;i++){
+			if(grades[i] > 5){
+				krediti++;
+			}
+		}
+		return (krediti * 6);
+
 	}
 	int grades[] = {7,7,10,10};
 }
